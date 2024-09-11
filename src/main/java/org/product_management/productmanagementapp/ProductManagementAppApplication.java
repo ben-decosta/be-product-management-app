873e7ec44ffc540cb8f567e3cd3bfdf4edd3e7ad
ben <benpanjaitan02@gmail.com>
2024-09-11 21:13:05 +0700

    Initial commit

---
+++ src/main/java/org/product_management/productmanagementapp/ProductManagementAppApplication.java
@@ -0,0 +1,13 @@
+package org.product_management.productmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/product_management/productmanagementapp/ProductManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.product_management.productmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
